6f4c3b1e5f7d42b20716bf04e12e3f52863d4ae3
maheshchimmiri <maheshchimmiri@gmail.com>
2013-06-06 14:40:42 +0530

    index.php completed
    index.php completed

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ include_once("header.php");
             <h2>Welcome To <span class="greenheading">Group Infra</span></h2>
           </div>
           <div class="service-content">
-          	<font color="WHITE"><marquee behavior="alternate" style="background:green;"><p style="font-size:15px;">What We Belive</p></marquee></font>
-            <div><strong><p style="font-size:20px;">Tomorrow we will run faster,stretch out our arms farther.... And one fine morning- So we beat on,boats against the current,borne back ceaselessly into the success.</p></strong> 
-<font color="WHITE"><marquee behavior="alternate" style="background:green"><p style="font-size:15px;">We Welcome You</p></marquee></font>
+          	<font color="WHITE"><p style="font-size:15px;background:green;">What We Belive</p></font>
+            <div><strong><p style="font-size:12px;">Tomorrow we will run faster,stretch out our arms farther.... And one fine morning- So we beat on,boats against the current,borne back ceaselessly into the success.</p></strong> 
+<font color="WHITE"><p style="font-size:15px;background:green;">We Welcome You</p></font>
 
 <p style="font-size:15px;">To cater the needs, <b>Group Infra</b> is being established with a mission to provide quality services to the world of infrastructure.</p>
 
@@ -32,7 +32,11 @@ include_once("header.php");
           </div>
           <div class="service-content">
             <div> <img src="images/service-img.gif" alt="" class="floatimgleft" />
-Our Clients 
+            	<img src="images/service-img.gif" alt="" class="floatimgleft" />
+            	<img src="images/service-img.gif" alt="" class="floatimgleft" />
+            	<img src="images/service-img.gif" alt="" class="floatimgleft" />
+            	<img src="images/service-img.gif" alt="" class="floatimgleft" />
+            	<img src="images/service-img.gif" alt="" class="floatimgleft" />
              </div>
           </div>
         </div>

710d219b2741caf2ecff57ca23399ede98d0d107
maheshchimmiri <maheshchimmiri@gmail.com>
2013-06-12 20:28:49 +0530

    footer
    footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
   <div class="footer-inside">
-    <div class="footerlink"> Copyright (c) leelam.com. All rights reserved. Design by Leelam <a href="http://www.leelam.com" style="color:#FFFFFF;"></a></div>
+    <div class="footerlink"><p style="color:white;">Copyright (c)@2013 Groupinfra.co.in All rights reserved.</p> <a href="http://www.leelam.com" style="color:#FFFFFF;">Design by Localweb.co.in</a></div>
   </div>
 </div>
 <div class="clear"></div>

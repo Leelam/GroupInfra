7582a36a834e4bf5246338b20d7c46d6eabb66c0
Gopala Krishna <gm@leelam.com>
2013-06-12 22:24:38 +0530

    footer edited
    footer edited

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
   <div class="footer-inside">
-    <div class="footerlink"><p style="color:white;">Copyright (c)@2013 Groupinfra.co.in All rights reserved.</p> <a href="http://www.leelam.com" style="color:#FFFFFF;">Design by Localweb.co.in</a></div>
+    <div class="footerlink"><p style="color:white;">Copyright (c)@2013 Groupinfra.co.in All rights reserved.</p> <a href="http://www.localweb.co.in" style="color:#FFFFFF;">Design by Localweb.co.in</a></div>
   </div>
 </div>
 <div class="clear"></div>
